90cf315fe68d9c0cd513603868349535eb5ba3d4
Luis Felipe <felipenepocontato@gmail.com>
2026-05-20 20:53:12 -0300

    feat:adicionando arquitetura em cadadas Controller,service,repository, model

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,16 @@
+package dev.java10x.cadastroDeNinjas.Missoes;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MissoesController {
+    @GetMapping("/missoes")
+    public String missoes() {
+        return "Boas-vindas ao missoes";
+    }
+
+}
